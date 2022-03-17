845abeecadf669947fbdf91ceb422efdd7c8daac
Valeria Makarevich <valeriamakarevich@outlook.com>
2022-03-17 19:19:44 +0300

    выполнила дз

---
+++ Дз оаип вариант 5/Дз оаип вариант 5/Дз оаип вариант 5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    const int size = 20;
+    double arr[size];
+
+    int n; 
+    std::cout << " Введите число вещественных чисел n = ";
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Значение [" << i << "] = ";
+        cin >> arr[i];
+    }
+
+    double max = arr[0];
+    for (int i = 0; i < n; i++)
+        if (arr[i] > max)
+            max = arr[i];
+
+    std::cout << " Максимальное значение : " << max << std::endl;
+
+    return 0;
+}
---
+++ очереди/очереди/очереди.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <queue>
+
+sing namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "RUS");
+
+    std::queue <int> q;  // создали очередь q
+
+std:: cout << "Пользователь, пожалуйста введите 7 чисел: " << std::endl;
+
+    for (int h = 0; h < 7; h++) {
+        int a;
+
+        std::cin >> a;
+
+        q.push(a);  // добавляем в очередь элементы
+    }
+
+    std::cout <<std::endl;
+    std::cout << "Самый первый элемент в очереди: " << q.front() << std:: endl;  // выводим первый
+                                                                     // элемент очереди
+    q.pop();  // удаляем элемент из очереди
+
+    std::cout << "Новый первый элемент (после удаления): " << q.front() << std::endl;
+
+    if (!q.empty()) std::cout << "Очередь не пуста!";  // проверяем пуста ли очередь (нет)
+
+    system("pause");
+    return 0;
+}
+
